242012bb8b2309ecb6b702dc23f590db8d1246e9
Monserrat Díaz <monse@nubiral.com>
2022-10-13 11:36:15 -0300

    Create reducer function

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,14 @@
 import { createStore } from "redux";
 
+const reducerCounter = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "decrement") {
+    return { counter: state.counter - 1 };
+  }
+
+  return state;
+};
+
 const store = createStore(reducerCounter);
