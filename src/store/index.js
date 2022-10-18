86bb6023330b6f137a203b530c66e6e41503f9d2
Monserrat Díaz <monse@nubiral.com>
2022-10-18 16:20:59 -0300

    Create a slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
-const reducerCounter = (state = { counter: 0, showCounter: true }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+const reducerCounter = (state = initialState, action) => {
   if (action.type === "increment") {
     return { counter: state.counter + 1, showCounter: state.showCounter };
   }
