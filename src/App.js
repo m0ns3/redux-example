f27664627a572dda004c3638ae169e01f68f5a6c
Monserrat Díaz <monse@nubiral.com>
2022-10-19 12:23:01 -0300

    Read and dispatch from a new slice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useSelector } from "react-redux";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.auth.isAuthenticated);
   return (
     <>
       <Header />
-      <Auth />
-      <Counter />
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && (
+        <>
+          <Counter />
+          <UserProfile />
+        </>
+      )}
     </>
   );
 }
